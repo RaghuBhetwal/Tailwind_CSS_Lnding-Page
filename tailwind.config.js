56c3d5a0c71412b25d14d02064a221d3495badad
Raghu Bhetwal <88844603+RaghuBhetwal@users.noreply.github.com>
2024-01-10 21:19:48 +0100

    update all the file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./dist/index.html"],
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
